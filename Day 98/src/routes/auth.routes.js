514b798216d77be9da2a1ebca31920e8bc8a5c40
unknown <siddheshawasare7@gmail.com>
2026-03-08 23:40:18 +0530

    Day 98

---
+++ Day 98/src/routes/auth.routes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req,res) => {
+    const { name, email, password } = req.body;
+
+    const isUserAlreadyExists = await userModel.findOne({email});
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message : "User already exists with this Email address"
+        });
+    }
+
+    const user = await userModel.create({
+        name, email, password
+    });
+
+    const token = jwt.sign({
+            id : user._id
+        },process.env.JWT_SECRET
+    );
+
+    res.cookie("jwt_token", token);
+    
+    res.status(201).json({
+        message : "User Registered Suceessfully",
+        user,
+        token
+    });
+});
+
+authRouter.post("/login", async (req,res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user){
+        return res.status(404).json({
+            message : "User not found with this Email address"
+        })
+    }   
+
+    const isPasswordMatched = await user.password === password;
+    if(!isPasswordMatched){
+        return res.status(401).json({
+            message : "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+            id : user._id
+        },process.env.JWT_SECRET
+    );
+
+    res.cookie("jwt_token", token);
+
+    res.status(200).json({
+        message : "User Logged In successfully",
+        user
+    });
+});
+
+module.exports = authRouter;
