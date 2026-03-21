66ff1d4cf1bb587b015b5f63f2f470a6dad96894
unknown <siddheshawasare7@gmail.com>
2026-03-21 18:03:43 +0530

    InstaClone-Post Creation

--- Day 103/src/routes/post.routes.js
+++ Day 103/src/routes/post.routes.js
@@ -5,6 +5,8 @@ const upload = multer({ storage: multer.memoryStorage() })
 
 const postRouter = express.Router();
 
+/* POST /api/posts/ [Protected]
+ - req.body = { caption, imageFile } */
 postRouter.post("/", upload.single("Image"), postController.createPostController);
 
 module.exports = postRouter;
